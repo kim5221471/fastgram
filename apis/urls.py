0b57efc0e062ceb7343fcd8c8cbc286a61f93720
kim <kim5221471@naver.com>
2020-09-11 16:28:06 +0900

    userlogin api

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apis.views import UserCreateView
+from apis.views import UserCreateView,UserLoginView
 
 urlpatterns=[
     path('v1/users/create',UserCreateView.as_view(),name='apis_v1_user'),
+    path('v1/users/login/',UserLoginView.as_view(),name='apis_v1_user_login'),
 ]
--- apis/views.py
+++ apis/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.core.validators import validate_email,ValidationError
+from django.contrib.auth import authenticate,login
 
 class BaseView(view):
     @staticmethod
@@ -33,3 +35,19 @@ class UserCreateView(BaseView):
             return self.response(message='이미 존재하는 아이디입니다.',status=400)
 
         return self.response({'user.id':user.id})
+
+class UserLoginView(BaseView):
+    def post(self,request):
+        username=request.POST.get('username','')
+        if not username:
+            return self.response(message='아이디를 입력해주세요',status=400)
+        password=request.POST.get('password','')
+        if fnot password:
+            return self.response(message='비밀번호를 입력해주세요.',status=400)
+
+        user=authenticate(request,username=username,password=password)
+        if user is None:
+            return self.response(message='입력 정보를 확인해주세요.',status=400)
+        login(request,user)
+
+        return self.response
