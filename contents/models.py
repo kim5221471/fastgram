35d6ffa53bf1903869d001e14995da08ecaa3711
kim <kim5221471@naver.com>
2020-09-14 15:41:45 +0900

    콘텐츠 and 이미지 기본모델

--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class BaseModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    modified_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+class Content(BaseModel):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    text=models.TextField(default='')
+
+def image_upload_to(instance,filename):
+    ext=filename.split('.')[-1]
+    return os.path.join(instance.UPLOAD_PATH,"%s.%s" % (uuid.uuid4(),ext))
+
+class Image(BaseModel):
+    UPLOAD_PATH='user-upload'
+
+    content=models.ForeignKey(Content,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to=image_upload_to)
+    order=models.SmallIntegerField()
+
+    class Meta:
+        ordering=['order']
--- fastgram/urls.py
+++ fastgram/urls.py
@@ -16,12 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 from contents.views import HomeView
+
+class NonUserTemplateView(TemplateView):
+    def dispatch(self,request,*args,**kwargs):
+        if not request.user.is_anonymous:
+            return redirect('contents_home')
+        return super(NonUserTemplateView,self).dispatch(request,*args,**kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apis/',include('apis.urls')),
     path('',HomeView.as_view(),name='contents_home'),
-    path('login/',TemplateView.as_view(template_name='login.html'),name='login'),
+    path('login/',NonUserTemplateView.as_view(template_name='login.html'),name='login'),
+    path('register/',NonUserTemplateView.as_view(template_name='register.html'),name='register'),
 ]
