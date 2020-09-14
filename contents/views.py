e5aab14c93fe792bb95c2eee972878c36153ca14
kim <kim5221471@naver.com>
2020-09-14 10:14:04 +0900

    로그인 페이지 분리

--- contents/views.py
+++ contents/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 class HomeView(TemplateView):
+    @method_decorator(login_required)
+    def dispatch(self,request,*args,**kwargs):
+        return super(HomeView,self).dispatch(request,*args,**kwargs)
+
     template_name='home.html'
+
--- fastgram/urls.py
+++ fastgram/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.views.generic import TemplateView
 
 from contents.views import HomeView
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('apis/',include('apis.urls')),
     path('',HomeView.as_view(),name='contents_home'),
+    path('login/',TemplateView.as_view(template_name='login.html'),name='login'),
 ]
